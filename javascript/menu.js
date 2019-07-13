72e5fa9956d8b07da89097604b2bb91719b156c5
Guillermo Alonso Lima <guillermoalonso.lima@gmail.com>
2019-07-13 16:06:04 -0300

    gui: menus e paginas, abrindo pagina ao clicar em menu

---
+++ javascript/menu.js
@@ -0,0 +1,25 @@
+var menus = ["home","restaurantes","parques","bares","ofertas","transporte","eventos","contato"];
+
+
+/* Desaparece com todas as paginas(divs)
+ * Faz um loop do array de menus, que possui os nomes dos menus e páginas
+ * Encontra o div com o id correspondente
+ * Coloca um 'none' na opção display para desaparecer com o mesmo
+*/
+function desaparecerMenus(item,index){
+    //alert("item:"+item);
+    document.getElementById(item).style.display = "none";
+}
+
+/* Seleciona o menu clicado
+ * Desaparece com todos os menus
+ * Encontra o div com o id igual ao botão clicado, mas sem o 'Btn'
+ * Coloca um 'block' na opção display para mostra-lo
+*/
+function selecionarMenu(buttonId){
+    //alert("buttonId:"+buttonId);
+    menus.forEach(desaparecerMenus);
+    //alert("buttonId2:"+buttonId);
+    document.getElementById(buttonId).style.display = "block";
+   //alert("buttonId3:"+buttonId);
+}
